13d551d9159181fc40975c45e393a659245e1a87
Marcos <marcosugaldemartinez@gmail.com>
2023-10-02 15:59:43 +0200

    feat: app index configuration finished

--- inventario-backend/index.js
+++ inventario-backend/index.js
@@ -4,10 +4,27 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { db, cors: options } = require("./configs");
+const errors = require("./misc/errors");
 
 app.use(cors(options));
 app.use(express.json());
 app.use(cookieParser());
+
+const routes = require("./routes");
+
+app.use(routes(db));
+
+app.use((_, __, next) => {
+  next(errors[404]);
+});
+
+app.use(({ statusCode, error }, _, res, _) => {
+  res.status(statusCode).json({
+    success: false,
+    message: error.message,
+  });
+});
 
 app.listen(process.env.PORT, () =>
   console.info(`App listening at port ${process.env.PORT}`)
