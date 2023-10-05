1fddb3cc8a9668580a918b6556fe64bd4094862d
Marcos <marcosugaldemartinez@gmail.com>
2023-10-05 13:23:52 +0200

    feat: added queries to create new users and get users information

---
+++ inventario-backend/models/auth/index.js
@@ -0,0 +1,56 @@
+const { insertUser, selectByEmail } = require("./queries");
+
+const createUser = (db) => async (email, username, password) => {
+  try {
+    await db.query(insertUser(email, username, password));
+    return {
+      ok: true,
+    };
+  } catch (error) {
+    console.info("> Create user error: ", error.message);
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
+const selectUser = (db) => async (email, compareFn) => {
+  try {
+    const user = await db.maybeOne(selectByEmail(email));
+
+    if (!user)
+      return {
+        ok: false,
+        error_code: "wrong_data",
+      };
+
+    const areEqual = await compareFn(user.password);
+
+    if (!areEqual)
+      return {
+        ok: false,
+        error_code: "wrong_data",
+      };
+
+    return {
+      ok: true,
+      content: {
+        email: user.email,
+        username: user.username,
+        id: user.id,
+      },
+    };
+  } catch (error) {
+    console.info("> Select user error: ", error.message);
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
+module.exports = {
+  createUser,
+  selectUser,
+};
--- inventario-backend/models/auth/queries.js
+++ inventario-backend/models/auth/queries.js
@@ -8,6 +8,13 @@ const insertUser = (username, email, password) => sql.unsafe`
     )
 `;
 
+const selectByEmail = (email) => sql.unsafe`
+        SELECT email, username, password, id
+        FROM users
+        WHERE email LIKE ${email}
+`;
+
 module.exports = {
   insertUser,
+  selectByEmail,
 };
