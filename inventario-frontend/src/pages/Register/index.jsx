ca7a1901de0e22366700125307f3f1b9b1b0f076
Marcos <marcosugaldemartinez@gmail.com>
2023-10-09 21:29:10 +0200

    feat: added services to register page

--- inventario-frontend/src/pages/Register/index.jsx
+++ inventario-frontend/src/pages/Register/index.jsx
@@ -1,8 +1,27 @@
-// import { useForm } from 'react-hook-form'
-// import { useMutation } from 'react-query'
-// import { useLocation } from 'wouter'
+import { useForm } from 'react-hook-form'
+import { useMutation } from 'react-query'
+import { useLocation } from 'wouter'
+import { login } from '../../misc/templates'
 
 function Register() {
+  const {register, formState, handleSubmit} = useForm()
+
+  const { mutate } = useMutation({
+    mutationFn: auth.register,
+    onSuccess: (response) => {
+      console.log('Response: ', response)
+      if(response.success) setLocation('/login')
+    }
+  })
+
+  const [, setLocation] = useLocation()
+
+  const handleForm = (data) => {
+    console.info('Form data: ', data)
+    mutate(data)
+  }
+
+  const { errors } = login
 
   return (
     <>
