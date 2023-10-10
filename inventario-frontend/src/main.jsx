fec7028013f40130b3b7812257ff98ebe449faff
Marcos <marcosugaldemartinez@gmail.com>
2023-10-10 13:01:10 +0200

    feat: added .env file

--- inventario-frontend/src/main.jsx
+++ inventario-frontend/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Switch, Route } from 'wouter'
 import Register from './pages/Register/index.jsx'
 import Provider from './context/Provider.jsx'
+import Login from './pages/Login/index.jsx'
 
 const Main = () => {
   return (
     <Provider>
       <Switch>
         <Route path='/signup' component={Register} />
+        <Route path='/login' component={Login} />
       </Switch>
     </Provider>
   )
