bc1155b603116186952e6a0a80767406ba3792d2
Marcos <marcosugaldemartinez@gmail.com>
2023-10-11 15:36:44 +0200

    feat: added hook to insert product

--- inventario-frontend/src/hooks/index.js
+++ inventario-frontend/src/hooks/index.js
@@ -2,3 +2,4 @@ export { useUser } from "./useUser";
 export { useLogout } from "./useLogout";
 export { useProducts } from "./useProducts";
 export { useSuppliers } from "./useSupliers";
+export { useProdInsertion } from "./useProdInsertion";
---
+++ inventario-frontend/src/hooks/useProdInsertion.js
@@ -0,0 +1,17 @@
+import { useMutation } from "react-query";
+import { useLocation } from "wouter";
+import { products } from "../services";
+
+export const useProdInsertion = () => {
+  //   const queryClient = useQueryClient();
+  const [, setLocation] = useLocation();
+
+  const { mutate: insertProduct } = useMutation({
+    mutationFn: products.addProduct,
+    onSuccess: (data) => {
+      if (data.success) setLocation("/");
+      console.info("> Product insertion completed");
+    },
+  });
+  return insertProduct;
+};
