4ca7dd0ae148d372fa7cc2655a58b3fa9616e86c
Marcos <marcosugaldemartinez@gmail.com>
2023-10-20 09:31:52 +0200

    styles: added styles to update product page

--- inventario-frontend/src/pages/Updateproduct/index.jsx
+++ inventario-frontend/src/pages/Updateproduct/index.jsx
@@ -1,3 +1,4 @@
+import Styled from './styles'
 import { useForm } from "react-hook-form"
 import { useSuppliers, useOneProduct, useId, useProdUpdate } from "../../hooks"
 import { Link } from 'wouter'
@@ -16,50 +17,57 @@ function Updateproduct() {
     console.log('Product in update page: ', product)
 
     return (
-        <>
+        <Styled.Body>
         {product.data ? (
             <>
                 <h3>Updating product</h3>
-                <form onSubmit={handleSubmit(doUpdateProduct)}>
-                    <input type="text" name="productId" id="productId" {...register('productId', {required: true})} value={product?.data[0].id} />
-                    <input type="text" name="product_name" id="product_name" placeholder="Insert product name" {...register('product_name', {required: true})}  />
-                    <input type="text" name="product_description" id="product_description" placeholder="Insert product description" {...register('product_description', {required: true})} defaultValue={product?.data[0].product_description} />
-                    <input type="text" name="product_quantity" id="product_quantity" placeholder="Insert quantity provided" {...register('product_quantity', {required: true})} defaultValue={product?.data[0].product_quantity} />
-                    <label htmlFor="supliers">Select a supplier</label>
-                    <select name="suppliers" id="suppliers" {...register('_supplier_product_name', {required: true})} defaultValue={product?.data[0]._supplier_product_name} >
-                        {suppliers?.map((supplier) => (
-                            <Link href={`/suppliers/${supplier.id}`} key={supplier.supplier_name}>
-                                <option value={supplier?.supplier_name}>
-                                    {supplier.supplier_name}
-                                </option>
-                            </Link>
-                                
-                        ) )}
-                    </select>
-                    <label htmlFor="sup">Select supplier id value</label>
-                    <select name="sup" id="sup" {...register('supplier_id', {required: true})} defaultValue={product?.data[0]._supplier_product_name} >
-                        {suppliers?.map((supplier) => (
-                            <Link href={`/suppliers/${supplier.id}`} key={supplier.id}>
-                                <option value={supplier?.id}>
-                                    {supplier.supplier_name}
-                                </option>
-                            </Link>                          
-                        ) )}
-                    </select>
-    {/*                 
-                    <input type="text" name="_supplier_product_name" id="_supplier_product_name" placeholder="Inserte proveedor deproducto *" {...register('_supplier_product_name', {required: true})} />
-                    <input type="text" name="supplier_id" id="supplier_id" placeholder="Insert supplier ID" {...register('supplier_id', {required: true})} />
-                    */}
-                    <input type="submit" value='Update' />
-                    <input type="button" value='Back' onClick={goBack} />
-                </form>
+                <Styled.Form onSubmit={handleSubmit(doUpdateProduct)}>
+                    <Styled.Inputs>
+                        <Styled.Texts>
+                            <Styled.Input type="text" name="productId" id="productId" {...register('productId', {required: true})} value={product?.data[0].id} />
+                            <Styled.Input type="text" name="product_name" id="product_name" placeholder="Insert product name" {...register('product_name', {required: true})}  />
+                            <Styled.Input type="text" name="product_description" id="product_description" placeholder="Insert product description" {...register('product_description', {required: true})} defaultValue={product?.data[0].product_description} />
+                            <Styled.Input type="text" name="product_quantity" id="product_quantity" placeholder="Insert quantity provided" {...register('product_quantity', {required: true})} defaultValue={product?.data[0].product_quantity} />
+                        </Styled.Texts>
+                        <br />
+                        <Styled.Selectors>
+                            <Styled.Field>
+                                <label htmlFor="supliers">Select a supplier</label>
+                                <select name="suppliers" id="suppliers" {...register('_supplier_product_name', {required: true})} defaultValue={product?.data[0]._supplier_product_name} >
+                                    {suppliers?.map((supplier) => (
+                                        <Link href={`/suppliers/${supplier.id}`} key={supplier.supplier_name}>
+                                            <option value={supplier?.supplier_name}>
+                                                {supplier.supplier_name}
+                                            </option>
+                                        </Link>
+                                            
+                                    ) )}
+                                </select>
+                            </Styled.Field>
+                            <Styled.Field>
+                                <label htmlFor="sup">Select supplier id value</label>
+                                <select name="sup" id="sup" {...register('supplier_id', {required: true})} defaultValue={product?.data[0]._supplier_product_name} >
+                                    {suppliers?.map((supplier) => (
+                                        <Link href={`/suppliers/${supplier.id}`} key={supplier.id}>
+                                            <option value={supplier?.id}>
+                                                {supplier.supplier_name}
+                                            </option>
+                                        </Link>                          
+                                    ) )}
+                                </select>
+                            </Styled.Field>
+                        </Styled.Selectors>
+                    </Styled.Inputs>
+                    <Styled.Buttons>
+                        <Styled.Update type="submit" value='Update' />
+                        <Styled.Back type="button" value='Back' onClick={goBack} />
+                    </Styled.Buttons>
+                </Styled.Form>
             </>
         ) : (
             <p>Loading product information...</p>
         )}
-
-            <p>*Insert an existing supplier</p>
-        </>
+        </Styled.Body>
     )
 }
 
---
+++ inventario-frontend/src/pages/Updateproduct/styles.jsx
@@ -0,0 +1,92 @@
+import { styled } from 'styled-components'
+
+const Body = styled.section`
+    height: 90vh;
+    width: 95vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: #7DD181;
+    height: 500px;
+    width: 550px;
+    border-radius: 15px;
+    box-shadow: 10px 10px 11px 2px rgba(0,0,0,0.75);
+`
+
+const Inputs = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const Texts = styled.div`
+    width: 550px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`
+
+const Input = styled.input`
+    border-radius: 10px;
+    width: 35%;
+    margin: 10px;
+`
+
+const Selectors = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
+const Field = styled.section`
+    display: flex;
+    flex-direction: column;
+`
+
+const Buttons = styled.div`
+    width: 550px;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const Update = styled.input`
+    border-radius: 10px;
+    width: 90px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #38a3a5;
+        color: white;
+        transition: 0.2s;
+    }
+`
+
+const Back = styled.input`
+    border-radius: 10px;
+    width: 90px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #38a3a5;
+        color: white;
+        transition: 0.2s;
+    }
+`
+
+export default {
+    Body,
+    Form,
+    Inputs,
+    Texts,
+    Input,
+    Selectors,
+    Field, 
+    Buttons,
+    Update,
+    Back
+}
