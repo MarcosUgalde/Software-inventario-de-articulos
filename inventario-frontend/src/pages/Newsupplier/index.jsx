3be79517d460b658f7fd9a340ca4749b96ac4e86
Marcos <marcosugaldemartinez@gmail.com>
2023-10-20 14:36:06 +0200

    style: added styles to new supplier page

--- inventario-frontend/src/pages/Newsupplier/index.jsx
+++ inventario-frontend/src/pages/Newsupplier/index.jsx
@@ -1,3 +1,4 @@
+import Styled from './styles'
 import { useForm } from 'react-hook-form'
 import { useSupInsertion } from '../../hooks'
 
@@ -10,16 +11,18 @@ function Newsupplier() {
     const doRegisterSup = useSupInsertion()
 
     return (
-        <>
+        <Styled.Body>
             <h1>Insert supplier information</h1>
-            <form onSubmit={handleSubmit(doRegisterSup)}>
-                <input type="text" name="supplier_name" id="supplier_name" placeholder="Insert supplier name" {...register('supplier_name', {required: true})} />
-                <input type="text" name="contact_sup" id="contact_sup" placeholder="Insert supplier contact information" {...register('contact_sup', {required: true})} />
-                <input type="text" name="" id="" placeholder="Insert a short supplier description" {...register('supplier_description', {required: true})}/>
-                <input type="submit" value='Add Supplier' />
-                <input type="button" value='Back' onClick={goBack}/>
-            </form>
-        </>
+            <Styled.Form onSubmit={handleSubmit(doRegisterSup)}>
+                <Styled.Input type="text" name="supplier_name" id="supplier_name" placeholder="Insert supplier name" {...register('supplier_name', {required: true})} />
+                <Styled.Input type="text" name="contact_sup" id="contact_sup" placeholder="Insert supplier contact information" {...register('contact_sup', {required: true})} />
+                <Styled.Input type="text" name="" id="" placeholder="Insert a short supplier description" {...register('supplier_description', {required: true})}/>
+                <Styled.Buttons>
+                    <Styled.Add type="submit" value='Add Supplier' />
+                    <Styled.Back type="button" value='Back' onClick={goBack}/>
+                </Styled.Buttons>
+            </Styled.Form>
+        </Styled.Body>
     )
 }
 
---
+++ inventario-frontend/src/pages/Newsupplier/styles.jsx
@@ -0,0 +1,66 @@
+import { styled } from 'styled-components'
+
+const Body = styled.section`
+    height: 90vh;
+    width: 95vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Form = styled.section`
+    background-color: #4DA1A9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 500px;
+    width: 550px;
+    border-radius: 15px;
+    box-shadow: 10px 10px 11px 2px rgba(0,0,0,0.75);
+`
+
+const Input = styled.input`
+    border-radius: 10px;
+`
+
+const Buttons = styled.div`
+    width: 550px;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const Add = styled.input`
+    background-color: #c5edac;
+    border-radius: 10px;
+    width: 90px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #22333b;
+        color: white;
+        transition: 0.2s;
+    }
+`
+
+const Back = styled.input`
+    background-color: #c5edac;
+    border-radius: 10px;
+    width: 90px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #22333b;
+        color: white;
+        transition: 0.2s;
+    }
+`
+
+export default {
+    Body,
+    Form,
+    Input,
+    Buttons,
+    Add,
+    Back
+}
