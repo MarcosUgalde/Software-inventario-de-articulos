09db2c856d41ce1640eb47c70dd9161407e2aecc
Marcos <marcosugaldemartinez@gmail.com>
2023-10-20 14:43:10 +0200

    style: added styles to update supplier page

--- inventario-frontend/src/pages/UpdateSupplier/index.jsx
+++ inventario-frontend/src/pages/UpdateSupplier/index.jsx
@@ -1,3 +1,4 @@
+import Styled from './styles'
 import { useForm } from "react-hook-form"
 import { useId, useOneSupplier, useSupplierUpdate } from '../../hooks'
 
@@ -12,18 +13,22 @@ function Updatesupplier() {
     const doSupplierUpdate = useSupplierUpdate()
     
     return (
-        <>
+        <Styled.Body>
         {supplier.data ? (
             <>
                 <h3>Update supplier</h3>
-                <form onSubmit={handleSubmit(doSupplierUpdate)}>
-                    <input type="text" name="supplierId" id="supplierId" {...register('supplierId', {required: true})} value={supplier?.data[0].id} />
-                    <input type="text" name="supplier_name" id="supplier_name" placeholder="Insert supplier name" {...register('supplier_name', {required: true})} defaultValue={supplier?.data[0].supplier_name} />
-                    <input type="text" name="contact_sup" id="contact_sup" placeholder="Insert supplier contact information" {...register('contact_sup', {required: true})} defaultValue={supplier?.data[0].contact_sup} />
-                    <input type="text" name="" id="" placeholder="Insert a short supplier description" {...register('supplier_description', {required: true})} defaultValue={supplier?.data[0].supplier_description} />
-                    <input type="submit" value='Update Supplier' />
-                    <input type="button" value='Back' onClick={goBack}/>
-                </form>
+                <Styled.Form onSubmit={handleSubmit(doSupplierUpdate)}>
+                    <Styled.Texts>
+                        <Styled.Input type="text" name="supplierId" id="supplierId" {...register('supplierId', {required: true})} value={supplier?.data[0].id} />
+                        <Styled.Input type="text" name="supplier_name" id="supplier_name" placeholder="Insert supplier name" {...register('supplier_name', {required: true})} defaultValue={supplier?.data[0].supplier_name} />
+                        <Styled.Input type="text" name="contact_sup" id="contact_sup" placeholder="Insert supplier contact information" {...register('contact_sup', {required: true})} defaultValue={supplier?.data[0].contact_sup} />
+                        <Styled.Input type="text" name="" id="" placeholder="Insert a short supplier description" {...register('supplier_description', {required: true})} defaultValue={supplier?.data[0].supplier_description} />
+                    </Styled.Texts>
+                    <Styled.Buttons>
+                        <Styled.Update type="submit" value='Update' />
+                        <Styled.Back type="button" value='Back' onClick={goBack}/>
+                    </Styled.Buttons>
+                </Styled.Form>
             </>
         ) : (
             <>
@@ -32,7 +37,7 @@ function Updatesupplier() {
         )
         
         }
-        </>
+        </Styled.Body>
     )
 }
 
--- inventario-frontend/src/pages/UpdateSupplier/styles.jsx
+++ inventario-frontend/src/pages/UpdateSupplier/styles.jsx
@@ -1,3 +1,76 @@
-// import { styled } from 'styled-components'
+import { styled } from 'styled-components'
 
-// export default {}
+const Body = styled.section`
+    height: 90vh;
+    width: 95vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Form = styled.section`
+    background-color: #4DA1A9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 500px;
+    width: 550px;
+    border-radius: 15px;
+    box-shadow: 10px 10px 11px 2px rgba(0,0,0,0.75);
+`
+
+const Texts = styled.div`
+    width: 550px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`
+
+const Input = styled.input`
+    border-radius: 10px;
+    width: 35%;
+    margin: 10px;
+`
+
+const Buttons = styled.div`
+    width: 550px;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const Update = styled.input`
+    background-color: #c5edac;
+    border-radius: 10px;
+    width: 90px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #22333b;
+        color: white;
+        transition: 0.2s;
+    }
+`
+
+const Back = styled.input`
+    background-color: #c5edac;
+    border-radius: 10px;
+    width: 90px;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #22333b;
+        color: white;
+        transition: 0.2s;
+    }
+`
+
+export default {
+    Body,
+    Form,
+    Texts,
+    Input,
+    Buttons,
+    Update,
+    Back
+}
